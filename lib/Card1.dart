f5abd99d5cd06c7e4583091ff44a297e402865b9
eoeefosa <eoeefosa@gmail.com>
2022-06-21 15:38:45 +0100

    updated card1.dart
    added comment in build widget

--- lib/Card1.dart
+++ lib/Card1.dart
@@ -11,12 +11,16 @@ class Card1 extends StatelessWidget {
     return Center(
       child: Container(
         padding: const EdgeInsets.all(16),
-        constraints: const BoxConstraints.expand(width: 350, height: 450),
+        constraints: const BoxConstraints.expand(width: 350, height: 500),
+        // Box decorator describes how to draw a box
         decoration: const BoxDecoration(
+          // decorationImage tell the box to paint an image
           image: DecorationImage(
+            // AssetImage gives image link
             image: AssetImage('assets/mag1.png'),
             fit: BoxFit.cover,
           ),
+          // corner radius
           borderRadius: BorderRadius.all(Radius.circular(10.0)),
         ),
       ),
