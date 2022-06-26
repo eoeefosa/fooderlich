92b6370e272a0674cb2588dafd82c59f7c165e25
eoeefosa <eoeefosa@gmail.com>
2022-06-26 12:44:51 +0100

    added icon button in authurs_card

--- lib/authors_card.dart
+++ lib/authors_card.dart
@@ -18,18 +18,33 @@ class AuthorCard extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.all(16.0),
       child: Row(
+        // todo add alli
         children: [
-          CircleImage(
-            imageProvider: imageProvider,
-            imageRadius: 26,
-          ),
-          const SizedBox(width: 8.0),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          Row(
             children: [
-              Text(authorName, style: FooderlichTheme.lightTextTheme.headline2),
-              Text(title, style: FooderlichTheme.lightTextTheme.headline3),
+              CircleImage(
+                imageProvider: imageProvider,
+                imageRadius: 26,
+              ),
+              const SizedBox(width: 8.0),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(authorName,
+                      style: FooderlichTheme.lightTextTheme.headline2),
+                  Text(title, style: FooderlichTheme.lightTextTheme.headline3),
+                ],
+              ),
             ],
+          ),
+          IconButton(
+            icon: Icon(Icons.favorite_border),
+            iconSize: 30,
+            color: Colors.blue[400],
+            onPressed: () {
+              const snackBar = SnackBar(content: Text('Favorite pressed'));
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            },
           )
         ],
       ),
